c134a63b1598522461762c03b32e58f9443dfe3f
Jared Palmer <jaredpalmer>
2016-06-16 16:18:28 -0400

    refactor with babel on server

--- modules/client/index.js
+++ modules/client/index.js
@@ -4,9 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-import './favicon.ico'
-import './robots.txt'
-
 const rootEl = document.getElementById('root')
 
 ReactDOM.render(
